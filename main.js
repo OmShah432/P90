c0894258b181598723b6464417c4338166973ab4
OmShah432 <115511358+OmShah432@users.noreply.github.com>
2023-09-25 10:47:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 =document.getElementById("number1").value;
+    actualy_answer = parseInt(number1) * parseInt(number2);
+
+    
+}
